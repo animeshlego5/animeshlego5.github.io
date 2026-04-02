83f9b8474a301a843acb04517c3d7331c41947f9
Animesh Gosain <126562373+animeshlego5@users.noreply.github.com>
2026-04-02 12:20:49 +0530

    refactor: removed registry.config and cleaned up site.config

--- src/config/site.ts
+++ src/config/site.ts
@@ -19,17 +19,11 @@ export const MAIN_NAV: NavItem[] = [
     title: "Portfolio",
     href: "/",
   },
-  // {
-  //   title: "Sponsors",
-  //   href: "/sponsors",
-  // },
 ];
 
 export const GITHUB_USERNAME = "animeshlego5";
 export const SOURCE_CODE_GITHUB_REPO = "animeshlego5/animeshlego5.github.io";
 export const SOURCE_CODE_GITHUB_URL =
   "https://github.com/animeshlego5/animeshlego5.github.io";
-
-// SPONSORSHIP_URL removed - GitHub Sponsors not set up for this account
 
 export const UTM_PARAMS = {};
